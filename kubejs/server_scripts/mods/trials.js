53a453eabfb1f7e21e56401a1da669562968a00b
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2025-03-03 20:53:30 -0600

    Add back Everycompat, Lithostitched, and Trial Chamber backport

--- kubejs/server_scripts/mods/trials.js
+++ kubejs/server_scripts/mods/trials.js
@@ -1,41 +1,39 @@
-// Erroring, will figure out later
-
-// //Trial Chamber backport
-// if (Platform.isLoaded('trials')) {
-//     ServerEvents.lowPriorityData(event => {
+//Trial Chamber backport
+if (Platform.isLoaded('trials')) {
+    ServerEvents.lowPriorityData(event => {
         
-//         //Make a Trial Processor using IntegratedAPI and Lithostitched
-//         //Integrated API's waterlog fix processor is used to fix blocks being waterlogged when the structure generates over water
-//         //Lithostitched is used to swap blocks while copying over properties. (without it this processor would be almost 3000 lines long)
-//         event.addJson("trials:worldgen/processor_list/generic", {
-//             "processors": [
-//             {
-//                 "processor_type": "integrated_api:waterlogging_fix_processor"
-//             },
-//             {
-//                 "processor_type": "lithostitched:block_swap",
-//                 "blocks": {
-//                     "minecraft:waxed_copper_block": "kubejs:trial_copper_block",
-//                     "minecraft:waxed_cut_copper": "kubejs:trial_cut_copper",
-//                     "trials:waxed_chiseled_copper": "kubejs:trial_chiseled_copper",
-//                     "trials:waxed_copper_grate": "kubejs:trial_copper_grate",
-//                     "minecraft:waxed_cut_copper_stairs": "kubejs:trial_cut_copper_stairs",
-//                     "minecraft:waxed_cut_copper_slab": "kubejs:trial_cut_copper_slab",
-//                     "minecraft:waxed_oxidized_copper": "kubejs:trial_oxidized_copper_block",
-//                     "minecraft:waxed_oxidized_cut_copper": "kubejs:trial_oxidized_cut_copper",
-//                     "trials:waxed_chiseled_copper_oxidized": "kubejs:trial_oxidized_chiseled_copper",
-//                     "trials:waxed_copper_grate_oxidized": "kubejs:trial_oxidized_copper_grate",
-//                     "minecraft:waxed_oxidized_cut_copper_stairs": "kubejs:trial_oxidized_cut_copper_stairs",
-//                     "minecraft:waxed_oxidized_cut_copper_slab":"kubejs:trial_oxidized_cut_copper_slab" 
-//                 }
-//             }
-//             ]
-//         })
+        //Make a Trial Processor using IntegratedAPI and Lithostitched
+        //Integrated API's waterlog fix processor is used to fix blocks being waterlogged when the structure generates over water
+        //Lithostitched is used to swap blocks while copying over properties. (without it this processor would be almost 3000 lines long)
+        event.addJson("trials:worldgen/processor_list/generic", {
+            "processors": [
+            {
+                "processor_type": "integrated_api:waterlogging_fix_processor"
+            },
+            {
+                "processor_type": "lithostitched:block_swap",
+                "blocks": {
+                    "minecraft:waxed_copper_block": "kubejs:trial_copper_block",
+                    "minecraft:waxed_cut_copper": "kubejs:trial_cut_copper",
+                    "trials:waxed_chiseled_copper": "kubejs:trial_chiseled_copper",
+                    "trials:waxed_copper_grate": "kubejs:trial_copper_grate",
+                    "minecraft:waxed_cut_copper_stairs": "kubejs:trial_cut_copper_stairs",
+                    "minecraft:waxed_cut_copper_slab": "kubejs:trial_cut_copper_slab",
+                    "minecraft:waxed_oxidized_copper": "kubejs:trial_oxidized_copper_block",
+                    "minecraft:waxed_oxidized_cut_copper": "kubejs:trial_oxidized_cut_copper",
+                    "trials:waxed_chiseled_copper_oxidized": "kubejs:trial_oxidized_chiseled_copper",
+                    "trials:waxed_copper_grate_oxidized": "kubejs:trial_oxidized_copper_grate",
+                    "minecraft:waxed_oxidized_cut_copper_stairs": "kubejs:trial_oxidized_cut_copper_stairs",
+                    "minecraft:waxed_oxidized_cut_copper_slab":"kubejs:trial_oxidized_cut_copper_slab" 
+                }
+            }
+            ]
+        })
 
-//         //Loot Tables are changed in the data folder.
+        //Loot Tables are changed in the data folder.
         
-//         //We need to change the type of loot tables to 'chest' so that emi Loot chooses to render it
-//         //The loot tables still work in trial vaults when they're set to the 'chest' type
-//         //Even if Lootjs were good in this version, we would still likely need to use a datapack to change the loot type
-//     })
-// }
+        //We need to change the type of loot tables to 'chest' so that emi Loot chooses to render it
+        //The loot tables still work in trial vaults when they're set to the 'chest' type
+        //Even if Lootjs were good in this version, we would still likely need to use a datapack to change the loot type
+    })
+}
