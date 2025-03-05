ebca3ce67d7dea0d61ac48ae1f1ad2e0a04afcc5
roidrole <83431927+roidrole@users.noreply.github.com>
2025-03-05 12:21:11 -0500

    Remove useless event parameter in functions

--- kubejs/server_scripts/mods/create_central_kitchen.js
+++ kubejs/server_scripts/mods/create_central_kitchen.js
@@ -20,12 +20,12 @@ if(Platform.isLoaded("create_central_kitchen")) {
 		event.remove({id:'create_central_kitchen:mixing/tomato_sauce'})
 		event.recipes.createMixing(Fluid.of("create_central_kitchen:tomato_sauce", 125), ["farmersdelight:tomato"]).heated()
 
-		let CreateSoup = (event, soup, fluid, ingredients, amountMade, dish) => {
+		let CreateSoup = (soup, fluid, ingredients, amountMade, dish) => {
 			event.recipes.createMixing(Fluid.of(fluid, amountMade), ingredients).heated()
 			event.recipes.createEmptying([dish, Fluid.of(fluid, 250)], soup)
 			event.recipes.createFilling(soup, [Fluid.of(fluid, 250), dish])
 		}
-		let TCSoup = (event, soup, fluid, ingredients, amountMade, dish) => {
+		let TCSoup = (soup, fluid, ingredients, amountMade, dish) => {
 			if(ingredients.length == 1){event.custom({"type": "tconstruct:melting",
 				ingredient: Ingredient.of(ingredients[0]).withCount(1).toJson(),
 				result: {
@@ -46,23 +46,23 @@ if(Platform.isLoaded("create_central_kitchen")) {
 				cooling_time: 1
 			})
 		}
-		let KJSoup = (event, soup, fluid, ingredients, amountMade) => {
-			CreateSoup(event, soup, fluid, ingredients, amountMade, "minecraft:bowl")
-			TCSoup(event, soup, fluid, ingredients, amountMade, "minecraft:bowl")
+		let KJSoup = (soup, fluid, ingredients, amountMade) => {
+			CreateSoup(soup, fluid, ingredients, amountMade, "minecraft:bowl")
+			TCSoup(soup, fluid, ingredients, amountMade, "minecraft:bowl")
 		}
-		let KJDrink = (event, soup, fluid, ingredients, amountMade) => {
-			CreateSoup(event, soup, fluid, ingredients, amountMade, "minecraft:glass_bottle")
-			TCSoup(event, soup, fluid, ingredients, amountMade, "minecraft:glass_bottle")
+		let KJDrink = (soup, fluid, ingredients, amountMade) => {
+			CreateSoup(soup, fluid, ingredients, amountMade, "minecraft:glass_bottle")
+			TCSoup(soup, fluid, ingredients, amountMade, "minecraft:glass_bottle")
 		}
-		CreateSoup(event, "minecraft:mushroom_stew","tconstruct:mushroom_stew", ["2x #forge:mushrooms"], 250, "minecraft:bowl")
-		CreateSoup(event, "minecraft:rabbit_stew","tconstruct:rabbit_stew",[Fluid.of("tconstruct:mushroom_stew", 250),"minecraft:baked_potato",Fluid.of("kubejs:rabbit_soup", 250),"minecraft:carrot"], 250, "minecraft:bowl")
-		CreateSoup(event, "minecraft:beetroot_soup", "tconstruct:beetroot_soup", ["3x minecraft:beetroot"], 250, "minecraft:bowl")
-		TCSoup(event, "farmersdelight:tomato_sauce", "create_central_kitchen:tomato_sauce", ["farmersdelight:tomato"], 125, "minecraft:bowl")
+		CreateSoup("minecraft:mushroom_stew","tconstruct:mushroom_stew", ["2x #forge:mushrooms"], 250, "minecraft:bowl")
+		CreateSoup("minecraft:rabbit_stew","tconstruct:rabbit_stew",[Fluid.of("tconstruct:mushroom_stew", 250),"minecraft:baked_potato",Fluid.of("kubejs:rabbit_soup", 250),"minecraft:carrot"], 250, "minecraft:bowl")
+		CreateSoup("minecraft:beetroot_soup", "tconstruct:beetroot_soup", ["3x minecraft:beetroot"], 250, "minecraft:bowl")
+		TCSoup("farmersdelight:tomato_sauce", "create_central_kitchen:tomato_sauce", ["farmersdelight:tomato"], 125, "minecraft:bowl")
 		//Dishes
 		event.recipes.createDeploying("kubejs:pasta",["minecraft:bowl","farmersdelight:raw_pasta"])
 
 		//Sequenced Assembly
-		let meal = (event, output, input, dish, loop) => {
+		let meal = (output, input, dish, loop) => {
 			var trans = KJ("incomplete_"+dish.split(":")[1]+"_meal")
 			event.recipes.createSequencedAssembly([output], dish, input.map(element => {
 				switch(true){
@@ -87,64 +87,64 @@ if(Platform.isLoaded("create_central_kitchen")) {
 				}
 			})).transitionalItem(trans).loops(loop)
 		}
-		let bowl = (event, output, input, loop) => meal(event, output, input, "minecraft:bowl", loop)
-		let rice = (event, output, input, loop) => meal(event, output, input, "farmersdelight:cooked_rice", loop)
-		let nood = (event, output, input, loop) => meal(event, output, input, "kubejs:pasta", loop)
+		let bowl = (output, input, loop) => meal(output, input, "minecraft:bowl", loop)
+		let rice = (output, input, loop) => meal(output, input, "farmersdelight:cooked_rice", loop)
+		let nood = (output, input, loop) => meal(output, input, "kubejs:pasta", loop)
 
 		//All farmers delight foods
-		meal(event, "farmersdelight:sweet_berry_cheesecake", ["fluidTag:'forge:milk',250","minecraft:sweet_berries","minecraft:sweet_berries"], 'farmersdelight:pie_crust', 2)
-		meal(event, 'farmersdelight:apple_pie', ["minecraft:sugar", "minecraft:apple", "minecraft:wheat"], 'farmersdelight:pie_crust', 2)
-		if(Platform.isLoaded("thermal")){thermalChiller(event,"farmersdelight:melon_popsicle",[Fluid.of("create_central_kitchen:melon_juice", 250),"minecraft:stick"],4000)}
-		KJDrink(event, "farmersdelight:glow_berry_custard", "kubejs:glow_berry_custard", ["minecraft:sugar", "#forge:eggs","minecraft:glow_berries",Fluid.of("minecraft:milk",250)],250)
-		bowl(event, "farmersdelight:fruit_salad", ["minecraft:apple","#forge:berries","#kubejs:fruit_slices"],2)
-		bowl(event, "farmersdelight:mixed_salad", ["#forge:crops/cabbage","farmersdelight:tomato","minecraft:beetroot"],1)
-		bowl(event, "farmersdelight:nether_salad", ["minecraft:crimson_fungus","minecraft:warped_fungus"],1)
-		meal(event, "farmersdelight:barbecue_stick", ["farmersdelight:onion","farmersdelight:tomato","#kubejs:barbecue_stick_ingredient"],"minecraft:stick",1) 
-		meal(event, 'farmersdelight:mutton_wrap', ["press","#forge:cooked_mutton","#forge:crops/cabbage","farmersdelight:onion"],"minecraft:bread",1)
+		meal("farmersdelight:sweet_berry_cheesecake", ["fluidTag:'forge:milk',250","minecraft:sweet_berries","minecraft:sweet_berries"], 'farmersdelight:pie_crust', 2)
+		meal('farmersdelight:apple_pie', ["minecraft:sugar", "minecraft:apple", "minecraft:wheat"], 'farmersdelight:pie_crust', 2)
+		if(Platform.isLoaded("thermal")){thermalChiller(event, "farmersdelight:melon_popsicle",[Fluid.of("create_central_kitchen:melon_juice", 250),"minecraft:stick"],4000)}
+		KJDrink("farmersdelight:glow_berry_custard", "kubejs:glow_berry_custard", ["minecraft:sugar", "#forge:eggs","minecraft:glow_berries",Fluid.of("minecraft:milk",250)],250)
+		bowl("farmersdelight:fruit_salad", ["minecraft:apple","#forge:berries","#kubejs:fruit_slices"],2)
+		bowl("farmersdelight:mixed_salad", ["#forge:crops/cabbage","farmersdelight:tomato","minecraft:beetroot"],1)
+		bowl("farmersdelight:nether_salad", ["minecraft:crimson_fungus","minecraft:warped_fungus"],1)
+		meal("farmersdelight:barbecue_stick", ["farmersdelight:onion","farmersdelight:tomato","#kubejs:barbecue_stick_ingredient"],"minecraft:stick",1) 
+		meal('farmersdelight:mutton_wrap', ["press","#forge:cooked_mutton","#forge:crops/cabbage","farmersdelight:onion"],"minecraft:bread",1)
 		event.recipes.createMixing("2x farmersdelight:dumplings", ["#kubejs:dumpling_ingredients", "#forge:dough","#forge:salad_ingredients/cabbage","farmersdelight:onion"]).heated()
-		meal(event, "farmersdelight:stuffed_potato", ["#forge:cooked_beef",Fluid.of('minecraft:milk',250)],"minecraft:baked_potato",1)
+		meal("farmersdelight:stuffed_potato", ["#forge:cooked_beef",Fluid.of('minecraft:milk',250)],"minecraft:baked_potato",1)
 		event.recipes.createMixing("farmersdelight:cabbage_rolls", ["#forge:salad_ingredients/cabbage", "#farmersdelight:cabbage_roll_ingredients"]).heated()
-		rice(event, "2x farmersdelight:salmon_roll", ["farmersdelight:salmon_slice"], 2); 
-		rice(event, "2x farmersdelight:cod_roll", ["farmersdelight:cod_slice"], 2); 
-		meal(event, "farmersdelight:kelp_roll", ["farmersdelight:cooked_rice", "minecraft:dried_kelp"],"minecraft:carrot",2) 
+		rice("2x farmersdelight:salmon_roll", ["farmersdelight:salmon_slice"], 2); 
+		rice("2x farmersdelight:cod_roll", ["farmersdelight:cod_slice"], 2); 
+		meal("farmersdelight:kelp_roll", ["farmersdelight:cooked_rice", "minecraft:dried_kelp"],"minecraft:carrot",2) 
 		event.recipes.createMixing("farmersdelight:cooked_rice", ["minecraft:bowl", "farmersdelight:rice"]).heated()
-		bowl(event, "farmersdelight:bone_broth", ['#kubejs:broth_ingredients', 'press', '#forge:bones'],1)
-		bowl(event, "farmersdelight:beef_stew",[Fluid.of("kubejs:beef_soup", 250),"minecraft:potato","minecraft:carrot"],1); 
-		bowl(event, "farmersdelight:chicken_soup", [Fluid.of("minecraft:milk",250),"#forge:vegetables",Fluid.of("kubejs:chicken_soup", 250),"minecraft:carrot","#forge:crops/cabbage"],1)
-		bowl(event, "farmersdelight:vegetable_soup", ["minecraft:beetroot", "minecraft:potato", "#forge:crops/cabbage", 'press', "minecraft:carrot"],1)
-		bowl(event, "farmersdelight:fish_stew", ["farmersdelight:tomato", "farmersdelight:onion", 'press', "fluidTag:'forge:soups/fish', 250"],1)
-		rice(event, "farmersdelight:fried_rice",["#forge:eggs","minecraft:carrot","farmersdelight:onion"],1);
-		bowl(event, "farmersdelight:pumpkin_soup", [Fluid.of("minecraft:milk",250),"farmersdelight:pumpkin_slice",Fluid.of("kubejs:pork_soup", 250),"#forge:crops/cabbage"],1)
-		bowl(event, "farmersdelight:baked_cod_stew",["farmersdelight:tomato","#forge:eggs","minecraft:potato",Fluid.of("kubejs:cod_soup", 250)],1);
-		nood(event, "farmersdelight:noodle_soup",["minecraft:dried_kelp",Fluid.of("kubejs:pork_soup", 250),"farmersdelight:fried_egg"],1)
-		bowl(event, "farmersdelight:bacon_and_eggs", ["#forge:cooked_bacon", "#forge:cooked_eggs"], 2); 
-		nood(event, "farmersdelight:pasta_with_meatballs", ["farmersdelight:minced_beef",Fluid.of("create_central_kitchen:tomato_sauce", 250)],1)
-		nood(event, "farmersdelight:pasta_with_mutton_chop",["#forge:raw_mutton",Fluid.of("create_central_kitchen:tomato_sauce", 250)],1)
-		rice(event, "farmersdelight:mushroom_rice", ["#forge:vegetables", Fluid.of("tconstruct:mushroom_stew",250)],1)
-		rice(event, "farmersdelight:roasted_mutton_chops", ["farmersdelight:cooked_mutton_chops","minecraft:beetroot","farmersdelight:tomato"],1)
-		nood(event, "farmersdelight:vegetable_noodles", ["#forge:crops/cabbage","#forge:vegetables","minecraft:carrot","minecraft:brown_mushroom"],1)
-		rice(event, "farmersdelight:steak_and_potatoes",["minecraft:cooked_beef","farmersdelight:onion","minecraft:baked_potato"],1) 
-		bowl(event, "farmersdelight:ratatouille", ["#forge:vegetables", "farmersdelight:tomato", "farmersdelight:onion", "minecraft:beetroot","#forge:tools/knives"],1)
-		nood(event, "farmersdelight:squid_ink_pasta", ["#forge:raw_fishes","farmersdelight:tomato","fluidTag:'forge:ink',125"],1)
-		bowl(event, "farmersdelight:grilled_salmon",["#forge:cooked_fishes/salmon","minecraft:sweet_berries","#forge:crops/cabbage","farmersdelight:onion"],1)
-		bowl(event, "farmersdelight:roast_chicken_block",["minecraft:carrot","minecraft:carrot","minecraft:baked_potato","minecraft:baked_potato","farmersdelight:onion","#forge:eggs","minecraft:bread","minecraft:cooked_chicken"],1)
+		bowl("farmersdelight:bone_broth", ['#kubejs:broth_ingredients', 'press', '#forge:bones'],1)
+		bowl("farmersdelight:beef_stew",[Fluid.of("kubejs:beef_soup", 250),"minecraft:potato","minecraft:carrot"],1); 
+		bowl("farmersdelight:chicken_soup", [Fluid.of("minecraft:milk",250),"#forge:vegetables",Fluid.of("kubejs:chicken_soup", 250),"minecraft:carrot","#forge:crops/cabbage"],1)
+		bowl("farmersdelight:vegetable_soup", ["minecraft:beetroot", "minecraft:potato", "#forge:crops/cabbage", 'press', "minecraft:carrot"],1)
+		bowl("farmersdelight:fish_stew", ["farmersdelight:tomato", "farmersdelight:onion", 'press', "fluidTag:'forge:soups/fish', 250"],1)
+		rice("farmersdelight:fried_rice",["#forge:eggs","minecraft:carrot","farmersdelight:onion"],1);
+		bowl("farmersdelight:pumpkin_soup", [Fluid.of("minecraft:milk",250),"farmersdelight:pumpkin_slice",Fluid.of("kubejs:pork_soup", 250),"#forge:crops/cabbage"],1)
+		bowl("farmersdelight:baked_cod_stew",["farmersdelight:tomato","#forge:eggs","minecraft:potato",Fluid.of("kubejs:cod_soup", 250)],1);
+		nood("farmersdelight:noodle_soup",["minecraft:dried_kelp",Fluid.of("kubejs:pork_soup", 250),"farmersdelight:fried_egg"],1)
+		bowl("farmersdelight:bacon_and_eggs", ["#forge:cooked_bacon", "#forge:cooked_eggs"], 2); 
+		nood("farmersdelight:pasta_with_meatballs", ["farmersdelight:minced_beef",Fluid.of("create_central_kitchen:tomato_sauce", 250)],1)
+		nood("farmersdelight:pasta_with_mutton_chop",["#forge:raw_mutton",Fluid.of("create_central_kitchen:tomato_sauce", 250)],1)
+		rice("farmersdelight:mushroom_rice", ["#forge:vegetables", Fluid.of("tconstruct:mushroom_stew",250)],1)
+		rice("farmersdelight:roasted_mutton_chops", ["farmersdelight:cooked_mutton_chops","minecraft:beetroot","farmersdelight:tomato"],1)
+		nood("farmersdelight:vegetable_noodles", ["#forge:crops/cabbage","#forge:vegetables","minecraft:carrot","minecraft:brown_mushroom"],1)
+		rice("farmersdelight:steak_and_potatoes",["minecraft:cooked_beef","farmersdelight:onion","minecraft:baked_potato"],1) 
+		bowl("farmersdelight:ratatouille", ["#forge:vegetables", "farmersdelight:tomato", "farmersdelight:onion", "minecraft:beetroot","#forge:tools/knives"],1)
+		nood("farmersdelight:squid_ink_pasta", ["#forge:raw_fishes","farmersdelight:tomato","fluidTag:'forge:ink',125"],1)
+		bowl("farmersdelight:grilled_salmon",["#forge:cooked_fishes/salmon","minecraft:sweet_berries","#forge:crops/cabbage","farmersdelight:onion"],1)
+		bowl("farmersdelight:roast_chicken_block",["minecraft:carrot","minecraft:carrot","minecraft:baked_potato","minecraft:baked_potato","farmersdelight:onion","#forge:eggs","minecraft:bread","minecraft:cooked_chicken"],1)
 		event.recipes.createMixing("farmersdelight:stuffed_pumpkin_block", ["minecraft:pumpkin","minecraft:potato","farmersdelight:rice","farmersdelight:onion","minecraft:brown_mushroom","#forge:vegetables","#forge:berries"]).heated()
-		meal(event, "farmersdelight:honey_glazed_ham_block", ["farmersdelight:cooked_rice","minecraft:sweet_berries","minecraft:sweet_berries",Fluid.of('create:honey',125)],"farmersdelight:smoked_ham",2)
-		bowl(event, "farmersdelight:shepherds_pie_block",["minecraft:baked_potato","#forge:cooked_mutton","farmersdelight:onion",Fluid.of('minecraft:milk',125)],2)
-		bowl(event, "farmersdelight:rice_roll_medley_block",["farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll","farmersdelight:cod_roll","farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll","farmersdelight:cod_roll","farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll"],1)
-		bowl(event, "farmersdelight:dog_food",["farmersdelight:rice","fluidTag:'forge:stews/meat', 250","minecraft:bone_meal","minecraft:rotten_flesh"],1)
-		meal(event, "farmersdelight:horse_feed",["minecraft:apple","minecraft:apple","minecraft:golden_carrot"],"minecraft:hay_block",1); 
+		meal("farmersdelight:honey_glazed_ham_block", ["farmersdelight:cooked_rice","minecraft:sweet_berries","minecraft:sweet_berries",Fluid.of('create:honey',125)],"farmersdelight:smoked_ham",2)
+		bowl("farmersdelight:shepherds_pie_block",["minecraft:baked_potato","#forge:cooked_mutton","farmersdelight:onion",Fluid.of('minecraft:milk',125)],2)
+		bowl("farmersdelight:rice_roll_medley_block",["farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll","farmersdelight:cod_roll","farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll","farmersdelight:cod_roll","farmersdelight:kelp_roll_slice","farmersdelight:salmon_roll"],1)
+		bowl("farmersdelight:dog_food",["farmersdelight:rice","fluidTag:'forge:stews/meat', 250","minecraft:bone_meal","minecraft:rotten_flesh"],1)
+		meal("farmersdelight:horse_feed",["minecraft:apple","minecraft:apple","minecraft:golden_carrot"],"minecraft:hay_block",1); 
 		
 		//Other foods
-		if(Platform.isLoaded("forbidden_arcanus")){bowl(event, "forbidden_arcanus:bat_soup", ["minecraft:nether_wart",Fluid.of("tconstruct:mushroom_stew",125),"forbidden_arcanus:bat_wing"],1)}
-		if(Platform.isLoaded("aquaculture")){bowl(event,'aquaculture:turtle_soup',[Fluid.of("minecraft:water",250),'#aquaculture:turtle'],1)}
+		if(Platform.isLoaded("forbidden_arcanus")){bowl("forbidden_arcanus:bat_soup", ["minecraft:nether_wart",Fluid.of("tconstruct:mushroom_stew",125),"forbidden_arcanus:bat_wing"],1)}
+		if(Platform.isLoaded("aquaculture")){bowl('aquaculture:turtle_soup',[Fluid.of("minecraft:water",250),'#aquaculture:turtle'],1)}
 		
 		//If and when the mod gets updated
 		if(Platform.isLoaded("buddycardsexp")){
 			event.recipes.createCompacting('8x buddycardsexp:buddycookie', ['2x #forge:flour', Fluid.of("kubejs:buddybean_paste",250)])
-			KJSoup(event, "buddycardsexp:buddybean_paste", "kubejs:buddybean_paste", ['2x buddycardsexp:diced_buddybeans'], 125)
-			meal(event,'buddycardsexp:buddybean_pie',[Fluid.of("kubejs:buddybean_paste",250),'buddycardsexp:buddygummies',"minecraft:sugar"],"farmersdelight:pie_crust",2)
-			meal(event,'buddycardsexp:buddycard_pack_farmers',["minecraft:sugar",Fluid.of("kubejs:buddybean_paste", 250),'buddycardsexp:shredded_buddycard'],"farmersdelight:canvas",2)
+			KJSoup("buddycardsexp:buddybean_paste", "kubejs:buddybean_paste", ['2x buddycardsexp:diced_buddybeans'], 125)
+			meal('buddycardsexp:buddybean_pie',[Fluid.of("kubejs:buddybean_paste",250),'buddycardsexp:buddygummies',"minecraft:sugar"],"farmersdelight:pie_crust",2)
+			meal('buddycardsexp:buddycard_pack_farmers',["minecraft:sugar",Fluid.of("kubejs:buddybean_paste", 250),'buddycardsexp:shredded_buddycard'],"farmersdelight:canvas",2)
 		}
 	})
 }
