f26723d5b12a45dc7252495e5be9242de6bf41c0
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2025-03-09 23:13:21 -0500

    Fix the processing time yet again, hopefully for the final time

--- kubejs/server_scripts/recipes/metallurgy.js
+++ kubejs/server_scripts/recipes/metallurgy.js
@@ -133,13 +133,13 @@ ServerEvents.recipes(event => {
 		event.custom({
 			"type": "create:mixing",
 			"ingredients": [
-				{ "amount": 1, "fluid": fluid1 },
-				{ "amount": 1, "fluid": fluid2 }
+				{ "amount": 2, "fluid": fluid1 },
+				{ "amount": 2, "fluid": fluid2 }
 			],
 			"results": [
-				{ "amount": 1, "fluid": TC(fluidAlloy) }
+				{ "amount": 2, "fluid": TC(fluidAlloy) }
 			],
-			processingTime: 5
+			processingTime: 1
 		}).id(`kubejs:mixing/${fluidAlloy}_2`)
 	}
 	moltenAlloy('molten_brass', TC('molten_copper'), TC('molten_zinc'))
