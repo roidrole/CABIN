1abbb2f398e3cad7db60cf792fde2af94a3959ec
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2025-03-11 23:22:17 -0500

    Remove old 1.18 compatability files, we'll add them back after testing that they work

--- kubejs/startup_scripts/startup_compatability/systeams.js
+++
@@ -1,5 +0,0 @@
-// Thermal Systeams
-if (Platform.isLoaded("systeams")) {
-	global.randomiumBlacklist.push("systeams:compression_boiler")
-	global.randomiumBlacklist.push("systeams:disenchantment_boiler")
-}
